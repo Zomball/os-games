f7466fcc334801c16de1f353d79933cb0f328e46
Zomball <162007513+Zomball@users.noreply.github.com>
2026-03-10 17:37:05 +0700

    Delete os-onet-connect directory

--- os-onet-connect/utils.js
+++
@@ -1,9 +0,0 @@
-function querySelectorAllAsList(selectorName) {
-    let result = [];
-    let nodeList = document.querySelectorAll(selectorName);
-    for (let i = 0; i < nodeList.length; i++) {
-        result.push(nodeList.item(i));
-    }
-    return result;
-}
-export { querySelectorAllAsList };
